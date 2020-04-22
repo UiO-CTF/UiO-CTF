73e4bfa5860761da1166f8218f32094dc532733b
Greger Martinussen <greger.martinussen@protonmail.com>
2020-04-22 13:45:42 +0200

    Updated team members, schedule, description and UiO -> Oslo

--- src/shared/components/Footer.js
+++ src/shared/components/Footer.js
@@ -5,7 +5,7 @@ import styles from '../styles/components/footer.css';
 export const footer = html`
     <footer class="${styles.footer}">
         <div class="${styles.footerContainer}">
-            © 2019 UiO-CTF - Site developed and designed by <a target="_blank" rel="noopener" href="https://twitter.com/jacobrclevenger">Jacob Clevenger</a>
+            © 2019 Oslo-CTF - Site developed and designed by <a target="_blank" rel="noopener" href="https://twitter.com/jacobrclevenger">Jacob Clevenger</a>
         </div>
     </footer>
 `;
